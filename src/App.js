3d4939eb40a2aec85e22e451f6accfab5a1404e2
epshita-shima <epshitashima@gmail.com>
2022-12-07 12:17:34 +0600

    show color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Box, TextField } from "@mui/material";
 
 function App() {
+  const [color, setColor] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 style={{ color: "goldenrod" }}>Dynamic color</h2>
+      <div>
+        <Box
+          sx={{
+            width: 200,
+            height: 200,
+            margin: "auto",
+            border:'1px solid gray',
+            backgroundColor: color,
+          }}
+        />
+      </div>
+      <TextField
+        required
+        id="outlined-required"
+        label={color}
+        placeholder="Color name/code"
+        sx={{ marginTop: "20px", height: "30px" }}
+        onChange={(e) => {
+          setColor(e.target.value);
+        }}
+      />
     </div>
   );
 }
